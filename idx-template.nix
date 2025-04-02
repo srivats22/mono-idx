ca3b28b4e9da6acdf65323de89f1dfd1ef9b0928
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2025-04-01 21:23:23 -0400

    Fixing template

--- idx-template.nix
+++ idx-template.nix
@@ -4,10 +4,10 @@
   ];
   bootstrap = ''
     mkdir "$out"
-    mkdir -p "$out/.idx/" "$out/client" "$out/server"
+    mkdir -p "$out/.idx/" "$out/client/" "$out/server/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/client "$out/client"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/server "$out/server"
+    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/client/* "$out/client/"
+    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/server/* "$out/server/"
     chmod -R +w "$out"
   '';
 }
