613682bea17e8c3bffc9f32f02dd240b6d88f0c5
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2025-04-01 21:31:01 -0400

    Update idx-template.nix

--- idx-template.nix
+++ idx-template.nix
@@ -6,8 +6,8 @@
     mkdir "$out"
     mkdir -p "$out/.idx/" "$out/client/" "$out/server/"
     cp -rf ${./dev.nix} "$out/.idx/dev.nix"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/client/* "$out/client/"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/server/* "$out/server/"
+    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/client/ "$out/client/"
+    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/server/ "$out/server/"
     chmod -R +w "$out"
   '';
 }
