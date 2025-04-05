7fb5a4ecd5abf9c5ecf2e0e7a9b78bb64ab1cc50
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2025-04-05 17:05:27 -0400

    Updating template

--- idx-template.nix
+++ idx-template.nix
@@ -1,13 +1,11 @@
-{ pkgs, typescript ? false, ... }: {
+{ pkgs, typescript ? false, ... }:{
   packages = [
     pkgs.nodejs_20
   ];
-  bootstrap = ''
-    mkdir "$out"
-    mkdir -p "$out/.idx/" "$out/client/" "$out/server/"
-    cp -rf ${./dev.nix} "$out/.idx/dev.nix"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/client "$out/client/"
-    cp -rf ${./monorepo}${if typescript then "-ts" else ""}/server "$out/server/"
-    chmod -R +w "$out"
-  '';
+  bootstrap =
+    ''
+      cp -rf ${./monorepo}${if typescript then "-ts" else ""} "$WS_NAME"
+      chmod -R +w "$WS_NAME"
+      mv "$WS_NAME" "$out"
+    '';
 }
