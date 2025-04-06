2e49defaec6c884f7e8325e4ccc4b1c9364ac269
Srivats Venkataraman <42980667+srivats22@users.noreply.github.com>
2025-04-06 10:34:38 -0400

    Typescript template

--- idx-template.nix
+++ idx-template.nix
@@ -1,11 +1,17 @@
-{ pkgs, typescript ? false, ... }:{
+{ pkgs, typescript ? false, ... }: {
   packages = [
     pkgs.nodejs_20
   ];
   bootstrap =
     ''
       mkdir "$out"
-      shopt -s dotglob; cp -r ${./monorepo}/* "$out"
+      if [ "${typescript}" == "true" ]; then
+        shopt -s dotglob
+        cp -r ${./monorepo-ts}/* "$out"
+      else
+        shopt -s dotglob
+        cp -r ${./monorepo}/* "$out"
+      fi
       chmod -R +w "$out"
     '';
 }
